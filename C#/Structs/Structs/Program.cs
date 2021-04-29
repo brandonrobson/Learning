2c4a63f8e1bcd0d017f2709668dd51e636ed1028
vivalavita555 <brandon.robson2000@outlook.com>
2021-04-29 16:19:22 +0100

    C# Practice

---
+++ C#/Structs/Structs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Structs
+{
+    struct HighScoreEntry
+    {
+        public float score;
+        public string playerName;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HighScoreEntry topScore;
+            topScore.score = 0;
+            topScore.playerName = "";
+
+            Console.WriteLine(topScore.playerName + " " + topScore.score);
+            Console.ReadLine();
+        }
+    }
+}
